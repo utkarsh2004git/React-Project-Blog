5c42b42327c3968087f4e18c4af599e4d33ade65
ukarsh2004git <utkarshshahare2004@gmail.com>
2024-04-25 18:03:21 +0530

    backend and frontend succesfuly connected for registration

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,7 +1,10 @@
-import "./Register.css"
+import "./Register.css";
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Register = () => {
+    const navigate = useNavigate();
     const [user, setUser] = useState({
         name: "",
         email: "",
@@ -10,54 +13,63 @@ const Register = () => {
     });
 
     const handleInput = (e) => {
-        let name = e.target.name
-        let value = e.target.value
+        let name = e.target.name;
+        let value = e.target.value;
         setUser({
             ...user,
             [name]: value,
         });
-    }
-    
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        // alert(user);
         console.log(user);
-        try{
-            const response =await fetch(`http://localhost:3000/api/auth/register`,{
-                method:"POST",
-                headers:{
-                    'Content-Type':"application/json",
-
+        try {
+            const response = await fetch(`http://localhost:3000/api/auth/register`, {
+                method: "POST",
+                headers: {
+                    'Content-Type': "application/json",
                 },
-                body:JSON.stringify(user),
+                body: JSON.stringify(user),
             });
+            if (response.ok) {
+                setUser({
+                    name: "",
+                    email: "",
+                    password: "",
+                    gender: "",
+                });
+                navigate("/");
+            }
+
             console.log(response);
-        }catch(error){
-            console.log("register",error);
+        } catch (error) {
+            console.log("register", error);
         }
-    }
+    };
+
     return (
         <>
             <section>
                 <main>
                     <div className="h-screen section-registration pt-3 bg-slate-600">
                         <div className="font-bold text-center text-2xl text-white">SIGN UP</div>
-                        <div className="container w-2/5 flex-column items-center border-white border-4 rounded-lg p-3 px-5 bg-slate-200 mt-3 " >
-                            <form  onSubmit={handleSubmit}>
-                                <div class="form-group my-2">
-                                    <label htmlFor="name" className="font-semibold ">Name</label>
+                        <div className="container w-2/5 flex-column items-center border-white border-4 rounded-lg p-3 px-5 bg-slate-200 mt-3">
+                            <form onSubmit={handleSubmit}>
+                                <div className="form-group my-2">
+                                    <label htmlFor="name" className="font-semibold">Name</label>
                                     <input type="text" className="form-control rounded-md" onChange={handleInput}
                                         value={user.name} name="name" id="name" placeholder="Enter your name"
                                         required autoComplete="off" />
                                 </div>
-                                <div class="form-group my-2">
-                                    <label htmlFor="email" className="font-semibold ">Email</label>
+                                <div className="form-group my-2">
+                                    <label htmlFor="email" className="font-semibold">Email</label>
                                     <input type="email" className="form-control rounded-md" onChange={handleInput}
                                         value={user.email} name="email" id="email" placeholder="Enter your email"
-                                        required autoComplete="off"/>
+                                        required autoComplete="off" />
                                 </div>
-                                <div class="form-group my-2">
-                                    <label htmlFor="password" className="font-semibold ">Password</label>
+                                <div className="form-group my-2">
+                                    <label htmlFor="password" className="font-semibold">Password</label>
                                     <input type="password" className="form-control rounded-md" onChange={handleInput} name="password"
                                         value={user.password} id="password" placeholder="Enter your password"
                                         required autoComplete="off" />
@@ -73,7 +85,7 @@ const Register = () => {
                                 </div>
 
                                 <div className="text-center">
-                                    <button type="submit" className="btn btn-primary text-center bg-blue-600 mt-3" >Register</button>
+                                    <button type="submit" className="btn btn-primary text-center bg-blue-600 mt-3">Register</button>
                                 </div>
                                 <NavLink to="/login" className="font-semibold text-primary">Already Registered?</NavLink>
                             </form>
